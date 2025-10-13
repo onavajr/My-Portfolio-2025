951fa1ca7562619e26384e8e8b04e7ec513945d5
Orlando <galunggong14@gmail.com>
2025-10-12 23:15:11 -0700

    Save: update portfolio.html

--- singlefile.js
+++ singlefile.js
@@ -1,5 +1,13 @@
+ 
+ //Form submission handling
  document.getElementById('contactForm').addEventListener('submit', function(e) {
             e.preventDefault();
             document.getElementById('formMessage').textContent = 'Thank you for contacting me!';
             this.reset();
         });
+
+//dark mode toggle
+function darklight() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
