ea8bd182572f324ebfa97be7199b192c4fc93238
Orlando <galunggong14@gmail.com>
2025-10-15 15:57:21 -0700

    Revert "Add dark mode and contact form scripts"
    This reverts commit e01426af9b69bbabeeb93cf6f26d2cb1f5a66499.

--- contactform.js
+++
@@ -1,5 +0,0 @@
- document.getElementById('contactForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            document.getElementById('formMessage').textContent = 'Thank you for contacting me!';
-            this.reset();
-        });
--- darklight.js
+++
@@ -1,4 +0,0 @@
-function darklight() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
